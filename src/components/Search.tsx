8ac129e4a4e6af22e66514c337572899acacf2ca
krupalll <137489828+krupalll@users.noreply.github.com>
2024-07-02 20:55:26 +0530

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -26,7 +26,7 @@ const Search = ({
   return (
 
 
-  <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+  <section className="w-full md: h-[100vh] max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
     <Header />
 
     <div className="relative flex mt-10 md:mt-4">
